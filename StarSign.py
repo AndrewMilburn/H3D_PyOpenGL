d86afd9108c3a2fe4b30f3a6b46de6eaebeef41e
Andrew Milburn <sidbell.069@gmail.com>
2022-04-07 06:55:55 +0100

    Create StarSign.py
    Penny's answer to challenge - create a function to draw a star at a position with size
    then have a list of stars to draw in the game loop

---
+++ StarSign.py
@@ -0,0 +1,48 @@
+import pygame
+from pygame.locals import *
+from OpenGL.GL import *
+from OpenGL.GLU import *
+
+pygame.init()
+
+screen_width = 1280
+screen_height = 720
+
+screen = pygame.display.set_mode((screen_width, screen_height), DOUBLEBUF | OPENGL)
+pygame.display.set_caption('OpenGL Window from Python')
+
+
+def init_ortho():
+    glMatrixMode(GL_PROJECTION)
+    glLoadIdentity()
+    gluOrtho2D(0, 640, 0, 480)
+
+
+def draw_star(x, y, size):
+    glPointSize(size)
+    glBegin(GL_POINTS)
+    glVertex2i(x, y)
+    glEnd()
+
+
+done = False
+init_ortho()
+while not done:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+    glMatrixMode(GL_MODELVIEW)
+    glLoadIdentity()
+
+    draw_star(100, 400, 15)
+    draw_star(270, 290, 7)
+    draw_star(320, 240, 12)
+    draw_star(330, 120, 14)
+    draw_star(430, 200, 5)
+    draw_star(500, 170, 10)
+
+    pygame.display.flip()
+    pygame.time.wait(100)
+pygame.quit()
