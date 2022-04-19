85a12ccdec0e0989ebcf574775693d99cd11b6f7
Andrew Milburn <sidbell.069@gmail.com>
2022-04-19 17:32:19 +0100

    #13 Converting Coordinate Systems
    Writing a Utility Function to map the screen coords to the surface coords

--- PlotMouse.py
+++ PlotMouse.py
@@ -1,12 +1,17 @@
+# PlotMouse.py
+
 import pygame
 from pygame.locals import *
 from OpenGL.GL import *
 from OpenGL.GLU import *
+from Utils import  *
 
 pygame.init()
 
 screen_width = 1000
 screen_height = 800
+ortho_width = 640
+ortho_height = 480
 
 screen = pygame.display.set_mode((screen_width, screen_height), DOUBLEBUF | OPENGL)
 pygame.display.set_caption('Scribble in PyOpenGL')
@@ -15,13 +20,8 @@ pygame.display.set_caption('Scribble in PyOpenGL')
 def init_ortho():
     glMatrixMode(GL_PROJECTION)
     glLoadIdentity()
-    gluOrtho2D(0, 640, 0, 480)
+    gluOrtho2D(0, ortho_width, 0, ortho_height)
 
-
-# def plot_graph():
-#     glBegin(GL_POINTS)
-#     glVertex2f(0, 0)
-#     glEnd()
 
 def plot_point():
     glBegin(GL_POINTS)
@@ -40,7 +40,9 @@ while not done:
         if event.type == pygame.QUIT:
             done = True
         elif event.type == MOUSEBUTTONDOWN:
-            points.append(pygame.mouse.get_pos())
+            p = pygame.mouse.get_pos()
+            points.append((map_value(0, screen_width, 0, ortho_width, p[0]),
+                          map_value(0, screen_height, ortho_height, 0, p[1])))
 
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
     glMatrixMode(GL_MODELVIEW)
